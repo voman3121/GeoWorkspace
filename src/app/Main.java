450c9b22bf82ffe85525f0caf8151eb627a7bd28
voman3121 <vidhu71106@gmail.com>
2026-05-04 09:00:46 +0530

    final:cl

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,26 +1,27 @@
 package app;
 
 import db.DBConnection;
-import db.DBServer;
+//import db.DBServer;
 import db.SchemaInitializer;
 import ui.WorkspaceFrame;
 
 import javax.swing.*;
 
 public class Main {
- public static void main(String[] args) {
-     try {
-         DBServer.start();
-         DBConnection.initialize();
-         SchemaInitializer.initialize();
 
-         SwingUtilities.invokeLater(() -> {
-             WorkspaceFrame frame = new WorkspaceFrame();
-             frame.setVisible(true);
-         });
+    public static void main(String[] args) {
+        try {
+//            DBServer.start();
+            DBConnection.initialize();
+            SchemaInitializer.initialize();
 
-     } catch (Exception e) {
-         e.printStackTrace();
-     }
- }
+            SwingUtilities.invokeLater(() -> {
+                WorkspaceFrame frame = new WorkspaceFrame();
+                frame.setVisible(true);
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
